728fe36ff7a57ac69ab97eae9a16cbb280eb7a8c
vidiniz <viniciusdiniz3@gmail.com>
2019-02-23 07:15:43 -0300

    Master - Configurações em Geral

---
+++ PRT/PRT.Infrastructure.Data/Context/PRTContext.cs
@@ -0,0 +1,37 @@
+﻿using PRT.Domain.Entitites;
+using PRT.Infrastructure.Data.EntityConfig;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRT.Infrastructure.Data.Context
+{
+    public class PRTContext : DbContext
+    {
+        public PRTContext() : base("DBPRT")
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasColumnType("varchar"));
+
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasMaxLength(200));
+
+            modelBuilder.Configurations.Add(new UserConfiguration());
+        }
+    }
+}
---
+++ PRT/PRT.Infrastructure.Data/EntityConfig/UserConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using PRT.Domain.Entitites;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRT.Infrastructure.Data.EntityConfig
+{
+    public class UserConfiguration : EntityTypeConfiguration<User>
+    {
+        public UserConfiguration()
+        {
+            HasKey(u => u.Id);
+
+            Property(u => u.Name)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            Property(u => u.Email)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            Property(u => u.Password)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            Property(u => u.Status)
+                .IsRequired();
+        }
+    }
+}
