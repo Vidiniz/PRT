fe710664cf3c92fbda0a70ec32b883b2111282bb
vidiniz <viniciusdiniz3@gmail.com>
2019-02-23 07:56:31 -0300

    Alterações Repositorio

---
+++ PRT/PRT.Infrastructure.Data/Repositories/RepositoryBase.cs
@@ -0,0 +1,47 @@
+﻿using PRT.Domain.Interfaces;
+using PRT.Infrastructure.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace PRT.Infrastructure.Data.Repositories
+{
+    public class RepositoryBase<TEntity> : IDisposable, IRepositoryBase<TEntity> where TEntity : class
+    {
+        protected PRTContext DataBase = new PRTContext();
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return DataBase.Set<TEntity>().ToList();
+        }
+
+        public TEntity GetById(int id)
+        {
+            return DataBase.Set<TEntity>().Find(id);
+        }
+
+        public void Remove(TEntity obj)
+        {
+            DataBase.Set<TEntity>().Remove(obj);
+            DataBase.SaveChanges();
+        }
+
+        public void Save(TEntity obj)
+        {
+            DataBase.Set<TEntity>().Add(obj);
+            DataBase.SaveChanges();
+        }
+
+        public void Update(TEntity obj)
+        {
+            DataBase.Entry(obj).State = EntityState.Modified;
+            DataBase.SaveChanges();
+        }
+    }
+}
---
+++ PRT/PRT.Infrastructure.Data/Repositories/UserRepository.cs
@@ -0,0 +1,9 @@
+﻿using PRT.Domain.Entitites;
+using PRT.Domain.Interfaces;
+
+namespace PRT.Infrastructure.Data.Repositories
+{
+    public class UserRepository : RepositoryBase<User>, IUserRepository
+    {
+    }
+}
